50227ac0fa875e3bf96981ac523bb64b007ec985
Ninjeng <ninzenlama21@gmail.com>
2020-06-20 15:14:29 +0545

    Initial commit Navigation managed

---
+++ app/src/main/java/ActivityPackage/RegisterActivity.java
@@ -0,0 +1,16 @@
+package ActivityPackage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.theakatsuki.hiredevelopers.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/theakatsuki/hiredevelopers/ui/developer/DeveloperFragment.java
@@ -0,0 +1,23 @@
+package com.theakatsuki.hiredevelopers.ui.developer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.theakatsuki.hiredevelopers.R;
+
+
+public class DeveloperFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_developer, container, false);
+    }
+}
